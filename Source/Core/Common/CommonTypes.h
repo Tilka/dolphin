4c9f657f74c03f06de1fbb603172cab595e1c7b0
Tillmann Karras <tilkax@gmail.com>
2014-02-03 23:56:11 +0100

    Jit64[IL]: fix float conversions
    Floating-point is complicated...
    
    Some background: Denormals are floats that are too close to zero to be
    stored in a normalized way (their exponent would need more bits). Since
    they are stored unnormalized, they are hard to work with, even in
    hardware.  That's why both PowerPC and SSE can be configured to operate
    in faster but non-standard-conpliant modes in which these numbers are
    simply rounded ('flushed') to zero.
    
    Internally, we do the same as the PowerPC CPU and store all floats in
    double format. This means that for loading and storing singles we need a
    conversion. The PowerPC CPU does this in hardware. We previously did
    this using CVTSS2SD/CVTSD2SS. Unfortunately, these instructions are
    considered arithmetic and therefore flush denormals to zero if non-IEEE
    mode is active. This normally wouldn't be a problem since the next
    arithmetic floating-point instruction would do the same anyway but as it
    turns out some games actually use floating-point instructions for
    copying arbitrary data.
    
    My idea for fixing this problem was to use x87 instructions since the
    x87 FPU never supported flush-to-zero and thus doesn't mangle denormals.
    However, there is one more problem to deal with: SNaNs are automatically
    converted to QNaNs (by setting the most-significant bit of the
    fraction). I opted to fix this by manually resetting the QNaN bit of all
    values with all-1s exponent.

--- Source/Core/Common/CommonTypes.h
+++ Source/Core/Common/CommonTypes.h
@@ -35,11 +35,13 @@ typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef __uint128_t u128;
 
 typedef int8_t  s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
+typedef __int128_t s128;
 
 #endif
 // For using windows lock code
